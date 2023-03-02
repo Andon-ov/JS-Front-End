78126e7069b833b18b35b2b487ffa38bea9013b5
BloodTaint <Bloodtaint@yahoo.com>
2023-03-02 13:44:08 +0200

    DOM and Events - More Exercises

--- 11.DOM and Events - Exercises/06. Table - Search Engine/solution.js
+++ 11.DOM and Events - Exercises/06. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+    let input = document.getElementById('searchField')
+    let rows = document.querySelectorAll(' tbody tr')
 
-   }
+    function onClick() {
+
+        for (let row of rows) {
+            row.classList.remove('select')
+            if (row.innerHTML.includes(input.value)) {
+                row.classList.add('select')
+            }
+        }
+        input.value = ''
+    }
 }
--- 11.DOM and Events - Exercises/07. Format the Text/solution.js
+++ 11.DOM and Events - Exercises/07. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+    let input = document.getElementById('input')
+    let output = document.getElementById('output')
+    let sentences = input.value.split('.').filter(s => s.length !== 0)
+
+
+    while (sentences.length > 0) {
+
+        let textForP = sentences.splice(0, 3).join('. ') + '.'
+        let p = document.createElement('p')
+
+        p.textContent = textForP
+        output.appendChild(p)
+
+    }
 }
