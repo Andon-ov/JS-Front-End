01befc567ad043729221c8d851dd8b5d390f2950
BloodTaint <Bloodtaint@yahoo.com>
2023-03-24 15:18:19 +0200

    edd Exercises

--- 13.HTTP and REST - Exercises/08.Book-library/app.js
+++ 13.HTTP and REST - Exercises/08.Book-library/app.js
@@ -1,21 +1,86 @@
-// load button
 function attachEvents() {
+    // load button
     document.getElementById("loadBooks").addEventListener("click", loadBooks);
 }
 
-
+// url
 const url = `http://localhost:3030/jsonstore/collections/books`;
+// tbody
 const tbody = document.querySelector("tbody");
+// form button
 const formBtn = document.querySelector('#form button');
-formBtn.addEventListener('click', addOrEditBook);
-
+formBtn.addEventListener('click', addBook);
+// title and author
+const title = document.querySelector('input[name="title"]');
+const author = document.querySelector('input[name="author"]');
+// form h3
+let formTitle = document.querySelector('#form h3');
 
 // load all books
 loadBooks();
 
-function addOrEditBook() {
+function addBook(event) {
+    let buttonState = event.target;
+    if (title.value === '' || author.value === '') {
+        return;
+    }
+    if (buttonState.textContent === 'Submit') {
+
+        let book = {
+            author: author.value,
+            title: title.value
+        };
+
+        createBook(book);
+        author.value = '';
+        title.value = '';
+        loadBooks();
+
+    } else {
+
+        let id = formBtn.value;
+
+        buttonState.textContent = 'Save';
+        formTitle.textContent = 'Edit FORM';
+
+        let book = {
+            author: author.value,
+            title: title.value
+        };
+        updateBook(id, book);
+
+        // const requestOptions = {
+        //     method: 'PUT',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify(book)
+        // };
+        // fetch(`${url}/${id}`, requestOptions)
+        //     .then(response => response.json());
+
+        buttonState.textContent = 'Submit';
+        formTitle.textContent = 'FORM';
+        author.value = '';
+        title.value = '';
+        loadBooks();
+    }
+}
+
+function editBook(event) {
+    let id = event.target.dataset.id;
+    formBtn.value = id;
+
+    formBtn.textContent = 'Save';
+    formTitle.textContent = 'Edit FORM';
+    let parent = event.target.parentElement.parentElement;
+
+    let tdTitle = parent.querySelector('td:nth-child(1)').textContent;
+    let tdaAuthor = parent.querySelector('td:nth-child(2)').textContent;
+    title.value = tdTitle;
+    author.value = tdaAuthor;
+    // parent.innerHTML = '';
 
 }
+
 
 // send request
 async function request(url, options) {
@@ -27,6 +92,7 @@ async function request(url, options) {
         });
     }
     const response = await fetch(url, options);
+
     if (response.ok !== true) {
         const error = await response.json();
         alert(error.message);
@@ -40,13 +106,11 @@ async function request(url, options) {
 // func load books
 async function loadBooks() {
     const books = await request(url);
-
     const result = Object.entries(books).map(([id, book]) => createRow(id, book));
     tbody.replaceChildren(...result);
 
-    [...document.getElementsByClassName('deleteBtn')].forEach(btn => {
-        btn.addEventListener('click',deleteBook())
-    });
+    Array.from(document.getElementsByClassName('deleteBtn')).forEach(b => b.addEventListener('click', deleteBook));
+    Array.from(document.getElementsByClassName('editBtn')).forEach(b => b.addEventListener('click', editBook));
 }
 
 // func create book
@@ -55,6 +119,8 @@ async function createBook(book) {
         method: "POST",
         body: JSON.stringify(book),
     });
+    loadBooks();
+
     return result;
 }
 
@@ -65,15 +131,18 @@ async function updateBook(id, book) {
         method: "PUT",
         body: JSON.stringify(book),
     });
+    loadBooks();
     return result;
 }
 
 // func delete book
-async function deleteBook() {
-    console.log();
+async function deleteBook(event) {
+    let id = event.target.dataset.id;
+
     const result = await request(`${url}/${id}`, {
         method: "DELETE",
     });
+    loadBooks();
     return result;
 }
 
@@ -84,8 +153,8 @@ function createRow(id, book) {
     row.innerHTML = `<td>${book.title}</td>
     <td>${book.author}</td>
     <td>
-    <button class ="editBtn">Edit</button>
-    <button class = "deleteBtn" data-id=${id}>Delete</button>
+        <button class ="editBtn" data-id=${id}>Edit</button>
+        <button class = "deleteBtn" data-id=${id}>Delete</button>
     </td>`;
     return row;
 
@@ -93,74 +162,3 @@ function createRow(id, book) {
 
 attachEvents();
 
-//TODO
-
-/*
-
-function attachEvents() {
-    document.getElementById("loadBooks").addEventListener("click", loadBooks);
-}
-
-
-
-const url = `http://localhost:3030/jsonstore/collections/books`;
-const tbody = document.querySelector("tbody");
-
-
-
-
-// const formBtn = document.querySelector('#form button');
-// formBtn.addEventListener('click', addOrEditBook);
-
-async function loadBooks() {
-    let res = await fetch(url);
-    if (res.status !== 200) {
-        throw new Error('Problem loading data');
-    }
-    let data = await res.json();
-    let entries = Object.entries(data);
-    tbody.innerHTML = '';
-    for (const [key, book] of entries) {
-        let row = createRow(key, book);
-        tbody.appendChild(row);
-    }
-    let allDeleteBtn = [...document.getElementsByClassName('deleteBtn')];
-    console.log(allDeleteBtn);
-    for (const btn in allDeleteBtn) {
-        btn.addEventListener('click', removeBook(e));
-
-        console.log(btn.target);
-
-    }
-}
-    // for (const deleteBtn of allDeleteBtn) {
-    //     deleteBtn.addEventListener('click', removeBook(e));
-    //     console.log(deleteBtn.target);
-    // }
-
-
-
-function createRow(id, book) {
-
-    const row = document.createElement('tr');
-    row.innerHTML = `<td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>
-        <button class='editBtn' >Edit</button>
-        <button class = 'deleteBtn' data-id=${id}>Delete</button>
-    </td>`;
-
-
-    return row;
-
-}
-function removeBook(e) {
-    console.log(e.target);
-}
-
-
-attachEvents();
-
-
-
-*/ 
