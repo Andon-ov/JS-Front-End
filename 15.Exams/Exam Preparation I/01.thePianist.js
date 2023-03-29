e2ff98c53c6a0cc1e13f0f76316ad3a04c46f150
BloodTaint <Bloodtaint@yahoo.com>
2023-03-29 21:10:52 +0300

    edd Exercises

--- 15.Exams/Exam Preparation I/01.thePianist.js
+++ 15.Exams/Exam Preparation I/01.thePianist.js
@@ -1,65 +1,71 @@
 function thePianist(input) {
-    let numberOfPieces = input.shift()
-    let pieces = []
-    input.splice(0, numberOfPieces).forEach(x => pieces.push(x.split('|')))
 
-    for (let text of input) {
-        if (text === 'Stop') {
-            break
+    let n = Number(input.shift());
+    let piecesCollection = {};
+
+    let commandParser = {
+        'Add': addPiece,
+        'Remove': removePiece,
+        'ChangeKey': changeKeyPiece
+    };
+
+    for (let i = 0; i < n; i++) {
+        let [piece, composer, key] = input.shift().split('|');
+        piecesCollection[piece] = {composer, key};
+    }
+
+    for (const inputLine of input) {
+        if (inputLine === 'Stop') {
+            break;
         }
 
-        if (text.startsWith('Add')) {
-            let [_, piece, composer, key] = text.split('|')
-            let havePiece = foundPieces(piece)
-            if (havePiece) {
-                console.log(`${piece} is already in the collection!`)
-            } else {
-                console.log(`${piece} by ${composer} in ${key} added to the collection!`)
-                pieces.push([piece, composer, key])
-            }
+        let commandTokens = inputLine.split('|');
+        let command = commandTokens.shift();
+        commandParser[command](...commandTokens);
+    }
 
-
-        } else if (text.startsWith('Remove')) {
-            let [_, piece] = text.split('|')
-            let havePiece = foundPieces(piece)
-            if (havePiece) {
-                let index = pieces.indexOf(havePiece)
-                pieces.splice(index, 1)
-                console.log(`Successfully removed ${piece}!`)
-            } else {
-                console.log(`Invalid operation! ${piece} does not exist in the collection.`)
-            }
-
-
-        } else if (text.startsWith('ChangeKey')) {
-            let [_, piece, newKey] = text.split('|')
-            let havePiece = foundPieces(piece)
-            if (havePiece) {
-                console.log(`Changed the key of ${havePiece[0]} to ${newKey}!`)
-                havePiece.splice(2, 1, newKey)
-
-                // havePiece.pop()
-                // havePiece.push(newKey)
-            } else {
-                console.log(`Invalid operation! ${piece} does not exist in the collection.`)
-            }
-
-        }
-
+    for (let piece in piecesCollection) {
+        const {key, composer} = piecesCollection[piece]
+        console.log(`${piece} -> Composer: ${composer}, Key: ${key}`)
 
     }
-    pieces.forEach(x => console.log(`${x[0]} -> Composer: ${x[1]}, Key: ${x[2]}`))
 
-    function foundPieces(piece) {
-        for (const pieceArr of pieces) {
-            if (piece === pieceArr[0])
-                return pieceArr
+    function addPiece(piece, composer, key) {
+        if (piecesCollection.hasOwnProperty(piece)) {
+            console.log(`${piece} is already in the collection!`);
 
+        } else {
+            console.log(`${piece} by ${composer} in ${key} added to the collection!`);
+            piecesCollection[piece] = {composer, key};
         }
     }
 
+    function removePiece(piece) {
+        if (piecesCollection.hasOwnProperty(piece)) {
+
+            delete piecesCollection[piece];
+            console.log(`Successfully removed ${piece}!`);
+
+        } else {
+            console.log(`Invalid operation! ${piece} does not exist in the collection.`);
+        }
+
+    }
+
+    function changeKeyPiece(piece, newKey) {
+        if (piecesCollection.hasOwnProperty(piece)) {
+
+            console.log(`Changed the key of ${piece} to ${newKey}!`);
+            piecesCollection[piece].key = newKey;
+
+        } else {
+
+            console.log(`Invalid operation! ${piece} does not exist in the collection.`);
+        }
+    }
 
 }
+
 
 thePianist([
     '3',
@@ -72,7 +78,7 @@ thePianist([
     'Remove|Clair de Lune',
     'ChangeKey|Moonlight Sonata|C# Major',
     'Stop'
-])
+]);
 thePianist([
     '4',
     'Eine kleine Nachtmusik|Mozart|G Major',
@@ -85,4 +91,81 @@ thePianist([
     'ChangeKey|Spring|C Major',
     'Add|Nocturne|Chopin|C# Minor',
     'Stop'
-])
+]);
+
+
+
+// function thePianist(input) {
+//     let numberOfPieces = input.shift()
+//     let pieces = []
+//     input.splice(0, numberOfPieces).forEach(x => pieces.push(x.split('|')))
+//     for (let text of input) {
+//         if (text === 'Stop') {
+//             break
+//         }
+//         if (text.startsWith('Add')) {
+//             let [_, piece, composer, key] = text.split('|')
+//             let havePiece = foundPieces(piece)
+//             if (havePiece) {
+//                 console.log(`${piece} is already in the collection!`)
+//             } else {
+//                 console.log(`${piece} by ${composer} in ${key} added to the collection!`)
+//                 pieces.push([piece, composer, key])
+//             }
+//         } else if (text.startsWith('Remove')) {
+//             let [_, piece] = text.split('|')
+//             let havePiece = foundPieces(piece)
+//             if (havePiece) {
+//                 let index = pieces.indexOf(havePiece)
+//                 pieces.splice(index, 1)
+//                 console.log(`Successfully removed ${piece}!`)
+//             } else {
+//                 console.log(`Invalid operation! ${piece} does not exist in the collection.`)
+//             }
+//         } else if (text.startsWith('ChangeKey')) {
+//             let [_, piece, newKey] = text.split('|')
+//             let havePiece = foundPieces(piece)
+//             if (havePiece) {
+//                 console.log(`Changed the key of ${havePiece[0]} to ${newKey}!`)
+//                 havePiece.splice(2, 1, newKey)
+//                 // havePiece.pop()
+//                 // havePiece.push(newKey)
+//             } else {
+//                 console.log(`Invalid operation! ${piece} does not exist in the collection.`)
+//             }
+//         }
+//     }
+//     pieces.forEach(x => console.log(`${x[0]} -> Composer: ${x[1]}, Key: ${x[2]}`))
+//     function foundPieces(piece) {
+//         for (const pieceArr of pieces) {
+//             if (piece === pieceArr[0])
+//                 return pieceArr
+//         }
+//     }
+// }
+
+// thePianist([
+//     '3',
+//     'Fur Elise|Beethoven|A Minor',
+//     'Moonlight Sonata|Beethoven|C# Minor',
+//     'Clair de Lune|Debussy|C# Minor',
+//     'Add|Sonata No.2|Chopin|B Minor',
+//     'Add|Hungarian Rhapsody No.2|Liszt|C# Minor',
+//     'Add|Fur Elise|Beethoven|C# Minor',
+//     'Remove|Clair de Lune',
+//     'ChangeKey|Moonlight Sonata|C# Major',
+//     'Stop'
+// ])
+// thePianist([
+//     '4',
+//     'Eine kleine Nachtmusik|Mozart|G Major',
+//     'La Campanella|Liszt|G# Minor',
+//     'The Marriage of Figaro|Mozart|G Major',
+//     'Hungarian Dance No.5|Brahms|G Minor',
+//     'Add|Spring|Vivaldi|E Major',
+//     'Remove|The Marriage of Figaro',
+//     'Remove|Turkish March',
+//     'ChangeKey|Spring|C Major',
+//     'Add|Nocturne|Chopin|C# Minor',
+//     'Stop'
+// ])
--- 15.Exams/Exam Preparation I/02. Scary Story/app.js
+++ 15.Exams/Exam Preparation I/02. Scary Story/app.js
@@ -151,3 +151,42 @@ function solve() {
 
     }
 }
+
+function createElement(type, parentNode, content, classes, id, attributes, htmlElement) {
+    const htmlElement = document.createElement(type);
+
+    if (content && useInnerHtml) {
+        htmlElement.innerHTML = content;
+
+    } else {
+
+        if (content && type !== 'input') {
+            htmlElement.textContent = content;
+
+        }
+        if (content && type === 'input') {
+            htmlElement.value = content;
+        }
+    }
+
+    if (classes && classes.length > 0) {
+        htmlElement.classList.add(...classes);
+    }
+
+    if (id) {
+        htmlElement.id = id;
+    }
+
+    if (attributes) {
+        for (let key in attributes) {
+            htmlElement.setAttribute(key, attributes[key]);
+            // htmlElement[key] = attributes[key];
+        }
+
+    }
+    if (parentNode) {
+        parentNode.appendChild(htmlElement);
+    }
+
+    return htmlElement;
+}
