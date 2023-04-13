773da9f0025e9ca460bf4d74358c222e6bf84f77
BloodTaint <Bloodtaint@yahoo.com>
2023-04-13 08:34:30 +0300

    add exresize

--- 15.Exams/Exam Second Exercises/02. Ski Lift/01. Ski lift-Resources/app.js
+++ 15.Exams/Exam Second Exercises/02. Ski Lift/01. Ski lift-Resources/app.js
@@ -1,120 +1,60 @@
-window.addEventListener('load', solve);
+window.addEventListener("load", solve);
 
 function solve() {
-
-    // get html elements
-    const body = document.getElementById('body');
-    const main = document.getElementById('main');
-    const form = document.querySelector('.container-text form');
-    const formBtn = document.getElementById('next-btn');
-    const ulInfoList = document.querySelector('.ticket-info-list');
-    const ulConfirmList = document.querySelector('.confirm-ticket');
-    form.addEventListener('click', nextStep);
-
-    // get form inputs
-    let ticket = {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+    const inputs = {
         firstName: document.getElementById("first-name"),
         lastName: document.getElementById("last-name"),
-        numberOfPeople: document.getElementById("people-count"),
+        peopleCount: document.getElementById("people-count"),
         fromData: document.getElementById("from-date"),
-        numberOfDay: document.getElementById("days-count"),
+        daysCount: document.getElementById("days-count"),
     };
+    const previewUl = document.querySelector('.ticket-info-list')
+    const nextBtn = document.getElementById("next-btn");
+    nextBtn.addEventListener("click", onAdd);
 
-    function nextStep(event) {
-        event.preventDefault();
-        // check inputs
-        let allInputIsCorrect = Object.values(ticket).every((input) => input.value !== '');
-        if (!allInputIsCorrect) {
+    const memory = {};
+    let id = 0;
+    function onAdd() {
+        const isValid = Object.values(inputs).every((x) => x.value !== "");
+        if (!isValid) {
             return;
         }
-        
-        // get all from ticket
-        let { firstName, lastName, numberOfPeople, fromData, numberOfDay } = ticket;
+        id += 1;
 
-        // make object copy
-        ticketInfo = {
+        memory[id] = {
             firstName: firstName.value,
             lastName: lastName.value,
-            numberOfPeople: numberOfPeople.value,
+            peopleCount: peopleCount.value,
             fromData: fromData.value,
-            numberOfDay: numberOfDay.value,
+            daysCount: daysCount.value,
         };
 
-        // create elements
-        const liElement = createElement('li', ulInfoList, null, ['ticket']);
-        const article = createElement('article', liElement);
-        const h3 = createElement('h3', article, `Name: ${firstName.value} ${lastName.value}`);
-        const pData = createElement('p', article, `From data: ${fromData.value}`);
-        const pDays = createElement('p', article, `For ${numberOfDay.value} days`);
-        const pPeople = createElement('p', article, `For ${numberOfPeople.value} people`);
-        const editBtn = createElement('button', liElement, 'Edit', ['edit-btn']);
-        editBtn.addEventListener('click', onEdit);
-        const continueBtn = createElement('button', liElement, 'Continue', ['continue-btn']);
-        continueBtn.addEventListener('click', onContinue);
 
-        // disable btn
-        formBtn.disabled = true;
 
-        // clear form inputs
-        form.reset();
-
-        function onEdit() {
-            // add data from copy object to inputs
-            for (const key in ticketInfo) {
-                ticket[key].value = ticketInfo[key];
-            }
-            // clear ul element
-            // ulInfoList.innerHTML = '';
-            this.parentNode.remove()
-
-            // unable btn
-            formBtn.removeAttribute('disabled');
-        }
-
-        function onContinue() {
-           
-            let liItem = this.parentNode
-            liItem.classList.remove('ticket')
-            liItem.classList.add('ticket-content')
-            const confirmBtn = createElement('button', liItem, 'Confirm', ['confirm-btn']);
-            confirmBtn.addEventListener('click', onConfirm);
-            const cancel = createElement('button', liItem, 'Cancel', ['cancel-btn']);
-            cancel.addEventListener('click', onCancel);
-
-            editBtn.remove();
-            continueBtn.remove();
-            ulConfirmList.appendChild(liItem);
-        }
-
-        function onConfirm() {
-            main.remove();
-            const h1 = createElement('h1', body, "Thank you, have a nice day! ", null, "thank-you");
-            const btn = createElement('button', body, "Back", null, "back-btn");
-            btn.addEventListener('click', () => {
-                location.reload();
-            });
-        }
-        function onCancel() {
-            this.parentNode.remove()
-
-            formBtn.removeAttribute('disabled');
-        }
     }
 
-
-    function createElement(type, parentNode, content, classes, id, attributes, useInnerHtml) {
+    function createElement(
+        type,
+        parentNode,
+        content,
+        classes,
+        id,
+        attributes,
+        useInnerHtml
+    ) {
         const htmlElement = document.createElement(type);
 
         if (content && useInnerHtml) {
             htmlElement.innerHTML = content;
-
         } else {
-
-            if (content && type !== 'input') {
+            if (content && type !== "input") {
                 htmlElement.textContent = content;
-
             }
-            if (content && type === 'input') {
+            if (content && type === "input") {
                 htmlElement.value = content;
             }
         }
@@ -132,7 +72,6 @@ function solve() {
                 htmlElement.setAttribute(key, attributes[key]);
                 // htmlElement[key] = attributes[key];
             }
-
         }
         if (parentNode) {
             parentNode.appendChild(htmlElement);
@@ -142,6 +81,143 @@ function solve() {
     }
 }
 
+// function solve() {
 
+//     // get html elements
+//     const body = document.getElementById('body');
+//     const main = document.getElementById('main');
+//     const form = document.querySelector('.container-text form');
+//     const formBtn = document.getElementById('next-btn');
+//     const ulInfoList = document.querySelector('.ticket-info-list');
+//     const ulConfirmList = document.querySelector('.confirm-ticket');
+//     form.addEventListener('click', nextStep);
 
+//     // get form inputs
+//     let ticket = {
+//         firstName: document.getElementById("first-name"),
+//         lastName: document.getElementById("last-name"),
+//         numberOfPeople: document.getElementById("people-count"),
+//         fromData: document.getElementById("from-date"),
+//         numberOfDay: document.getElementById("days-count"),
+//     };
 
+//     function nextStep(event) {
+//         event.preventDefault();
+//         // check inputs
+//         let allInputIsCorrect = Object.values(ticket).every((input) => input.value !== '');
+//         if (!allInputIsCorrect) {
+//             return;
+//         }
+
+//         // get all from ticket
+//         let { firstName, lastName, numberOfPeople, fromData, numberOfDay } = ticket;
+
+//         // make object copy
+//         ticketInfo = {
+//             firstName: firstName.value,
+//             lastName: lastName.value,
+//             numberOfPeople: numberOfPeople.value,
+//             fromData: fromData.value,
+//             numberOfDay: numberOfDay.value,
+//         };
+
+//         // create elements
+//         const liElement = createElement('li', ulInfoList, null, ['ticket']);
+//         const article = createElement('article', liElement);
+//         const h3 = createElement('h3', article, `Name: ${firstName.value} ${lastName.value}`);
+//         const pData = createElement('p', article, `From data: ${fromData.value}`);
+//         const pDays = createElement('p', article, `For ${numberOfDay.value} days`);
+//         const pPeople = createElement('p', article, `For ${numberOfPeople.value} people`);
+//         const editBtn = createElement('button', liElement, 'Edit', ['edit-btn']);
+//         editBtn.addEventListener('click', onEdit);
+//         const continueBtn = createElement('button', liElement, 'Continue', ['continue-btn']);
+//         continueBtn.addEventListener('click', onContinue);
+
+//         // disable btn
+//         formBtn.disabled = true;
+
+//         // clear form inputs
+//         form.reset();
+
+//         function onEdit() {
+//             // add data from copy object to inputs
+//             for (const key in ticketInfo) {
+//                 ticket[key].value = ticketInfo[key];
+//             }
+//             // clear ul element
+//             // ulInfoList.innerHTML = '';
+//             this.parentNode.remove()
+
+//             // unable btn
+//             formBtn.removeAttribute('disabled');
+//         }
+
+//         function onContinue() {
+
+//             let liItem = this.parentNode
+//             liItem.classList.remove('ticket')
+//             liItem.classList.add('ticket-content')
+//             const confirmBtn = createElement('button', liItem, 'Confirm', ['confirm-btn']);
+//             confirmBtn.addEventListener('click', onConfirm);
+//             const cancel = createElement('button', liItem, 'Cancel', ['cancel-btn']);
+//             cancel.addEventListener('click', onCancel);
+
+//             editBtn.remove();
+//             continueBtn.remove();
+//             ulConfirmList.appendChild(liItem);
+//         }
+
+//         function onConfirm() {
+//             main.remove();
+//             const h1 = createElement('h1', body, "Thank you, have a nice day! ", null, "thank-you");
+//             const btn = createElement('button', body, "Back", null, "back-btn");
+//             btn.addEventListener('click', () => {
+//                 location.reload();
+//             });
+//         }
+//         function onCancel() {
+//             this.parentNode.remove()
+
+//             formBtn.removeAttribute('disabled');
+//         }
+//     }
+
+//     function createElement(type, parentNode, content, classes, id, attributes, useInnerHtml) {
+//         const htmlElement = document.createElement(type);
+
+//         if (content && useInnerHtml) {
+//             htmlElement.innerHTML = content;
+
+//         } else {
+
+//             if (content && type !== 'input') {
+//                 htmlElement.textContent = content;
+
+//             }
+//             if (content && type === 'input') {
+//                 htmlElement.value = content;
+//             }
+//         }
+
+//         if (classes && classes.length > 0) {
+//             htmlElement.classList.add(...classes);
+//         }
+
+//         if (id) {
+//             htmlElement.id = id;
+//         }
+
+//         if (attributes) {
+//             for (let key in attributes) {
+//                 htmlElement.setAttribute(key, attributes[key]);
+//                 // htmlElement[key] = attributes[key];
+//             }
+
+//         }
+//         if (parentNode) {
+//             parentNode.appendChild(htmlElement);
+//         }
+
+//         return htmlElement;
+//     }
+// }
