22f3998670bde4c679f745f7a1d5d10192a31a0a
BloodTaint <Bloodtaint@yahoo.com>
2023-04-19 15:35:33 +0300

    add exresize

--- 14.HTTP and REST - More Exercises/03. Fisher-Game/src/login.js
+++ 14.HTTP and REST - More Exercises/03. Fisher-Game/src/login.js
@@ -1,44 +1,81 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", onLogin);
 
-const form = document.querySelector('form');
-form.addEventListener('submit', onLogin);
-document.getElementById('logout').style.display = 'none';
+document.getElementById("logout").style.display = "none";
 const url = `http://localhost:3030/users/login/`;
 
-async function onLogin(event) {
-    event.preventDefault();
-    // const formData = new FormData(form);
-    const formData = new FormData(event.target);
+function onLogin(ev) {
+    ev.preventDefault();
+    const formData = new FormData(ev.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
 
-    const email = formData.get('email');
-    const password = formData.get('password');
-   
-    try {
+    fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password }),
+    })
+        .then(res => {
+            if (!res.ok) {
+                const error = res.json();
+                throw new Error(error.message);
+            }
+            return res.json();
+        })
+        .then(data => {
 
-        const res = await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password })
+            const userData = {
+                email: data.email,
+                id: data._id,
+                token: data.accessToken
+            };
+            localStorage.setItem('userData', JSON.stringify(userData));
+            window.location = './index.html';
+        })
+        .catch(error => {
+            console.log(error.message);
+            alert(error);
+            form.reset();
         });
-        if (res.ok !== true) {
-
-            const error = await res.json();
-            throw new Error(error.message);
-        }
-
-        const data = await res.json();
-
-        const user = {
-            email: data.email,
-            id: data._id,
-            token: data.accessToken
-        };
-
-        localStorage.setItem('userData', JSON.stringify(user));
-        window.location = './index.html';
-
-    } catch (error) {
-        document.querySelector('form').reset();
-        alert(error.message);
-    }
-
 }
+
+// async function onLogin(event) {
+//     event.preventDefault();
+
+//     const formData = new FormData(event.target);
+
+//     const email = formData.get('email');
+//     const password = formData.get('password');
+
+//     try {
+
+//         const res = await fetch(url, {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({ email, password })
+//         });
+//         if (res.ok !== true) {
+
+//             const error = await res.json();
+//             throw new Error(error.message);
+//         }
+
+//         const data = await res.json();
+
+//         const user = {
+//             email: data.email,
+//             id: data._id,
+//             token: data.accessToken
+//         };
+
+//         localStorage.setItem('userData', JSON.stringify(user));
+//         window.location = './index.html';
+
+//     } catch (error) {
+//         document.querySelector('form').reset();
+//         alert(error.message);
+//     }
+
+// }
--- 14.HTTP and REST - More Exercises/03. Fisher-Game/src/register.js
+++ 14.HTTP and REST - More Exercises/03. Fisher-Game/src/register.js
@@ -1,54 +1,107 @@
 
 const form = document.querySelector('form');
-const url = 'http://localhost:3030/users/register';
-document.getElementById('logout').style.display = 'none'
-
-
 form.addEventListener('submit', onRegister);
 
-async function onRegister(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
+const url = 'http://localhost:3030/users/register';
+document.getElementById('logout').style.display = 'none';
 
-    const email = formData.get('email');
-    const password = formData.get('password');
-    const rePass = formData.get('rePass');
+function onRegister(ev) {
+    ev.preventDefault();
 
-    try {
+    const formData = new FormData(ev.currentTarget);
+    const { email, password, rePass } = Object.fromEntries(formData);
+    const validInputs = [...formData.values()].every(x => x !== "");
 
-        if (email === '' || password === '' || rePass === '') {
-            throw new Error('Input is not correct!');
-        }
-        if (password !== rePass) {
-            throw new Error('Password is not same with Repeat');
-        }
-
-        const res = await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password, rePass })
-        });
-
-        if (res.ok !== true) {
-            const error = await res.json();
-            throw new Error(error.message);
-        }
-
-        const data = await res.json();
-
-        const user = {
-            email: data.email,
-            id: data._id,
-            token: data.accessToken
-        };
-
-        localStorage.setItem('userData', JSON.stringify(user));
-        window.location = './index.html'
-
-    } catch (error) {
-        document.querySelector('form').reset();
-        alert(error.message);
-
+    if (!validInputs) {
+        alert('Input is not correct!');
+        return;
     }
 
+    if (password !== rePass) {
+        alert('Password and Repeat not equal!');
+        return;
+    }
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password, rePass })
+    })
+        .then(res => {
+            if (!res.ok) {
+                const error = res.json();
+                throw new Error(error.message);
+            }
+            return res.json();
+        })
+        .then(data => {
+
+            const user = {
+                email: data.email,
+                id: data._id,
+                token: data.accessToken
+            };
+
+            localStorage.setItem('userData', JSON.stringify(user));
+            window.location = './index.html';
+
+
+        })
+        .catch(error => {
+            alert(error.message);
+            form.reset();
+        });
+
+
+
 }
+
+// async function onRegister(event) {
+//     event.preventDefault();
+
+//     const formData = new FormData(event.target);
+
+//     const email = formData.get('email');
+//     const password = formData.get('password');
+//     const rePass = formData.get('rePass');
+
+//     try {
+
+//         if (email === '' || password === '' || rePass === '') {
+//             throw new Error('Input is not correct!');
+//         }
+//         if (password !== rePass) {
+//             throw new Error('Password is not same with Repeat');
+//         }
+
+//         const res = await fetch(url, {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({ email, password, rePass })
+//         });
+
+//         if (res.ok !== true) {
+//             const error = await res.json();
+//             throw new Error(error.message);
+//         }
+
+//         const data = await res.json();
+
+//         const user = {
+//             email: data.email,
+//             id: data._id,
+//             token: data.accessToken
+//         };
+
+//         localStorage.setItem('userData', JSON.stringify(user));
+//         window.location = './index.html';
+
+//     } catch (error) {
+//         document.querySelector('form').reset();
+//         alert(error.message);
+
+//     }
+
+// }
